abb149af163545c1c07e1d85e1a80425461bef26
Nathekip <130141751+Nathekip@users.noreply.github.com>
2024-03-21 18:18:01 +0100

    Update python-sql library

--- python_sql.py
+++ python_sql.py
@@ -6,13 +6,18 @@ identifiants = {'database':'ipam',
                 'host':'localhost',
                 'port':5432}
 
-def get(identifiants:dict[str, str], table:str) -> list[tuple]:
+def get(identifiants:dict[str, str], tables:list[str]) -> list[tuple]:
     with ps.connect(**identifiants) as connector:
         with connector.cursor() as cursor:
-            table_2 = ""
-            cursor.execute(f"""select * from public."{table}"
-                                inner join {table_2}
-                           """)
+            if len(tables) == 1:
+                cursor.execute(f"""
+                               select * from public."{tables[0]}"
+                                """)
+            else:
+                cursor.execute(f"""
+                               select * from public."{tables[0]}"
+                               inner join public."{tables[1]}" on public."{tables[0]}".{tables[1]}_id = public."{tables[1]}".id
+                               """)
             return cursor.fetchall()
 
 def put(identifiants:dict[str, str], table:str, data:dict[str,str]) -> bool:
@@ -35,12 +40,15 @@ def delete(identifiants:dict[str, str], table:str, id:int) -> bool:
                                 delete from public."IP_address" where id = {id}
                                 """)
                 connector.commit()
-                return True
+                return cursor.rowcount > 0
     except:
         return False
 
 if __name__ == '__main__':
     """User guide de la librairie"""
-    print(get(identifiants, "IP_address"))
+    print(get(identifiants, ["IP_address"]))
+    print(get(identifiants, ["subnet", "group"]))
     print(put(identifiants, table="IP_address", data={'id':6, 'address': "10.4.2.1"}))
-    print(delete(identifiants, "IP_address", 254))
+    print(delete(identifiants, "IP_address", 6))
+    print(put(identifiants, "subnet", {'id':5, 'first@':'10.0.0.2', 'gp_id':1}))
+    print(put(identifiants, "group", {'id':1, 'name':'Vannes'}))
