324ec72f13fc10c2c0d25f7a79474c17f6e1fd34
Nath <130141751+Nathekip@users.noreply.github.com>
2024-03-28 09:48:08 +0100

    Add files via upload

--- test_api_ipam.py
+++ test_api_ipam.py
@@ -4,54 +4,57 @@ from requests.structures import CaseInsensitiveDict
 import traceback
 
 def test_patch(path:str='/', data:dict[str, str]= {}) -> tuple[bool, dict]:
-    headers = CaseInsensitiveDict()
-    headers['Content-Type'] = 'application/json'
-    test = requests.patch( f'http://localhost:5000{path}', headers=headers, data=json.dumps(data))
-    return (f'Code {test.status_code}', json.loads(test.text))
+    try:
+        headers = CaseInsensitiveDict()
+        headers['Content-Type'] = 'application/json'
+        test = requests.patch( f'http://localhost:5000{path}', headers=headers, data=json.dumps(data))
+        return (f'Code {test.status_code}', json.loads(test.text))
+    except:
+        return traceback.print_exc()
 
 def test_get(path:str='/') -> tuple[bool, dict]:
-    headers = CaseInsensitiveDict()
-    headers['Content-Type'] = 'application/json'
-    test = requests.get( f'http://localhost:5000{path}', headers=headers)
-    return (f'Code {test.status_code}', json.loads(test.text))
+    try:
+        headers = CaseInsensitiveDict()
+        headers['Content-Type'] = 'application/json'
+        test = requests.get( f'http://localhost:5000{path}', headers=headers)
+        return (f'Code {test.status_code}', json.loads(test.text))
+    except:
+        return traceback.print_exc()
 
 def test_delete(path:str='/', id:int=0) -> tuple[bool, dict]:
-    headers = CaseInsensitiveDict()
-    headers['Content-Type'] = 'application/json'
-    test = requests.delete( f'http://localhost:5000{path}{id}', headers=headers)
-    return (f'Code {test.status_code}', json.loads(test.text))
+    try:
+        headers = CaseInsensitiveDict()
+        headers['Content-Type'] = 'application/json'
+        test = requests.delete( f'http://localhost:5000{path}{id}', headers=headers)
+        return (f'Code {test.status_code}', json.loads(test.text))
+    except:
+        return traceback.print_exc()
 
-def test_post(path:str='/', data:dict[str, str]= {}) -> tuple[bool, dict]:
-    headers = CaseInsensitiveDict()
-    headers['Content-Type'] = 'application/json'
-    test = requests.post( f'http://localhost:5000{path}', headers=headers, data=json.dumps(data))
-    return (f'Code {test.status_code}', json.loads(test.text))
+def test_post(path:str='/', data:dict[str, str]= {}) -> tuple[str, dict]:
+    try:
+        headers = CaseInsensitiveDict()
+        headers['Content-Type'] = 'application/json'
+        test = requests.post( f'http://localhost:5000{path}', headers=headers, data=json.dumps(data))
+        return (f'Code {test.status_code}', json.loads(test.text))
+    except:
+        return traceback.print_exc()
 
 ##### Tests Group
 
 # POST /group
-try:
-    t_post = test_post('/group/', {'id':2,
-                                   'name':'Vannes',
-                                   'parent_id':1,
-                                   'content_ids':[1,2,3,4],
-                                   'reading_roles':['dsi_local', 'superadmin'],
-                                   'writing_roles':['superadmin']})
-    print(f'POST /group/ {"{data}"} : {t_post}')
-except Exception as e:
-    traceback.print_exc()
+t_post = test_post('/group/', {'id':2,
+                                'name':'Vannes',
+                                'parent_id':1,
+                                'content_ids':[1,2,3,4],
+                                'reading_roles':['dsi_local', 'superadmin'],
+                                'writing_roles':['superadmin']})
+print(f'POST /group/ {{data}} : {t_post}')
 
 # PATCH /group
 
 # GET /group/{groupId}
-try:
-    groupes = test_get('/group/2')[1]
-    vannes = groupes[0]
-    test = vannes == [1, 'Vannes']
-except Exception as e:
-    print(e)
-    test = False
-print(f'GET /group/{{groupId}} : {test}')
+t_get = test_get('/group/2')
+print(f'GET /group/{{groupId}} : {t_get}')
 
 # GET /group/findByName
 try:
